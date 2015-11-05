148ad213f5e2b074bd7e9eccf2712d5c4890ddd8
NckHmml <NckHmml@users.noreply.github.com>
2015-11-05 15:33:34 +0900

    Added specs

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# challenge-pager-api
-Implement pager api
+# Information
+
+The goal of this challenge is to create a basic pager API, the specifications for this API are created by using 'api-first-spec' so these are already avalaible to you.
+
+As most of the specification is already defined in the spec files (see the spec folder), some steps might seem to be lacking information to run correctly, in these cases please read the spec file belonging to the step.
+
+Any custom configuration can be done in the config.json file.
+
+All calls done to the API shall be RESTful, all data shall be json.
+
+The [database](./data/database.db) only contains one table called "challenges", which consist out of the following columns:
+- Id (int, auto increment)
+- Owner (string)
+- Title (string)
+- Date (timestamp)
+
+The API should have the following options available that will be passed in the url as parameters:
+- Offset: This is an integer which defines the offset in the list, so it is possible to skip a certain amount.
+    - The default should be 0
+- Take: This is an integer which defines how many items to display.
+    - The default should be 10
+- OrderCol: This integer defines what column to order on.
+    - Default order is ascending
+    - 0 means Id (Default)
+    - 1 means Owner
+    - 2 means Title
+    - 3 means Date
+- OrderDesc: This booleans defines if the order is descending.
+    - Default is false
+- Filter: This string defines what the values should be filtered on, this applies to all columns except 'Id'
+- FromDate: This defines the date to start from.
+- ToDate: This defines the date to end from.
+
+The API should also return the following data:
+- Total: The total of items in the database
+- TotalFilter: The total of items after applying the filter
+
+You are free to use "Comments.md" for any kind of notes, for example:
+- Personal notes or problems
+- Information on how your code works
+- Any suggestions on what should be changed in the specifications.
+
+### Step 1
+Implement all basic features so a list of 10 challenges will be returned.
+
+### Step 2
+Implement the 'Offset' option
+
+### Step 3
+Implement the 'Take' option
+
+### Step 4
+Implement the 'OrderCol' and 'OrderDesc' options
+
+### Step 5 
+Implement the 'Filter' option
+
+### Step 6
+Implement the 'FromDate' and 'ToDate' options
---
+++ spec/list.spec.js
@@ -0,0 +1,146 @@
+﻿"use strict";
+
+var assert = require("chai").assert,
+    spec = require("api-first-spec"),
+    config = require("../config.json");
+
+var API = spec.define({
+    "endpoint": config.endpoints.list,
+    "method": "GET",
+    "request": {
+        "contentType": spec.ContentType.URLENCODED,
+        "params": {
+            "offset": "int",
+            "take": "int",
+            "orderCol": "int",
+            "orderDesc": "boolean",
+            "filter": "string",
+            "fromDate": "string",
+            "toDate": "string"
+        }
+    },
+    "response": {
+        "contentType": spec.ContentType.JSON,
+        "data": {
+            "total": "int",
+            "totalFilter": "int",
+            "challenges": [{
+                "owner": "string",
+                "title": "string",
+                "date": "datetime"
+            }]
+        },
+        "rules": {
+            "total": {
+                "required": true
+            },
+            "totalFilter": {
+                "required": true
+            },
+            "challenges": {
+                "required": true
+            },
+            "challenges.owner": {
+                "required": true
+            },
+            "challenges.title": {
+                "required": true
+            },
+            "challenges.date": {
+                "required": true,
+                "format": "YYYY-MM-DD"
+            }
+        }
+    }
+});
+
+describe("list challenges", function () {
+    var host = spec.host(config.host);
+
+    it("Step1 without parameters", function (done) {
+        host.api(API)
+            .params({})
+            .success(function (data, res) {
+                assert.equal(data.total, 27);
+                assert.equal(data.totalFilter, data.total);
+                assert.equal(data.challenges.length, 10);
+                assert.equal(data.challenges[0].title, "Challenge 1");
+                done();
+            });
+    });
+
+    it("Step2 with offset", function (done) {
+        host.api(API)
+            .params({
+                "offset": 5
+            })
+            .success(function (data, res) {
+                assert.equal(data.total, 27);
+                assert.equal(data.totalFilter, data.total);
+                assert.equal(data.challenges.length, 10);
+                assert.equal(data.challenges[0].title, "Challenge 6");
+                done();
+            });
+    });
+
+    it("Step3 with take", function (done) {
+        host.api(API)
+            .params({
+                "take": 15
+            })
+            .success(function (data, res) {
+                assert.equal(data.total, 27);
+                assert.equal(data.totalFilter, data.total);
+                assert.equal(data.challenges.length, 15);
+                assert.equal(data.challenges[0].title, "Challenge 1");
+                assert.equal(data.challenges[14].title, "API 5");
+                done();
+            });
+    });
+
+    it("Step4 with orderCol and orderDesc", function (done) {
+        host.api(API)
+            .params({
+                "orderCol": 2,
+                "orderDesc": true
+            })
+            .success(function (data, res) {
+                assert.equal(data.total, 27);
+                assert.equal(data.totalFilter, data.total);
+                assert.equal(data.challenges.length, 10);
+                assert.equal(data.challenges[0].title, "Framework 8");
+                assert.equal(data.challenges[9].title, "Challenge 8");
+                done();
+            });
+    });
+
+    it("Step5 with filter", function (done) {
+        host.api(API)
+            .params({
+                "filter": "mew"
+            })
+            .success(function (data, res) {
+                assert.equal(data.total, 27);
+                assert.equal(data.totalFilter, 8);
+                assert.equal(data.challenges.length, 8);
+                assert.equal(data.challenges[0].title, "Framework 1");
+                done();
+            });
+    });
+
+    it("Step6 with fromDate and toDate", function (done) {
+        host.api(API)
+            .params({
+                "fromDate": "2015-10-28",
+                "toDate": "2015-10-30",
+                "orderCol": 3,
+                "orderDesc": true
+            })
+            .success(function (data, res) {
+                assert.equal(data.challenges.length, 9);
+                assert.equal(data.challenges[0].title, "Challenge 8");
+                assert.equal(data.challenges[8].title, "Framework 6");
+                done();
+            });
+    })
+});
